592da5d897a1eb0dd72ff465a42ee7703ba661b2
Tom Veasey <tveasey@elastic.co>
2021-04-06 22:10:35 +0100

    apriori examples (WIP)

---
+++ examples/apriori/test_frequent_item_sets.py
@@ -0,0 +1,23 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+import utils.read_scripted_metric as read_scripted_metric
+
+def test_frequent_sets(user_name, password):
+    # Test that the sccripted metric produces similar results to the python implementation.
+    # The results are not identical because of the random sampling.
+
+    if user_name and password:
+        es = Elasticsearch(http_auth=(user_name, password))
+    else:
+        es = Elasticsearch()
+
+    query_body = read_scripted_metric.read('examples/apriori/scripted_metric_frequent_sets.txt')
+
+    result = Search.from_dict(query_body).using(es).index('apriori_demo').execute()
+
+    size = 1
+    for rulesk in result.aggregations.random_sample.frequent_sets.value:
+        print('FREQUENT_SETS(size=' + str(size) + ')')
+        for key,support in rulesk.to_dict().items():
+            print('  ', key, 'support', support)
+        size = size + 1
---
+++ utils/read_scripted_metric.py
@@ -0,0 +1,35 @@
+import json
+
+def recursive_find_and_replace(json, placeholder, replace):
+    for key,value in json.items():
+        if value == placeholder:
+            json[key] = replace
+        elif isinstance(json[key], dict):
+            recursive_find_and_replace(json[key], placeholder, replace)
+
+def read(file_name):
+    # We need special handling of triple quoted strings which are not supported in the
+    # JSON reader. These just need to be copied as strings into the request body for
+    # the aggregation.
+
+    scripted_metric = open(file_name, 'r').read()
+
+    split_scripted_metric = scripted_metric.split('"""')
+
+    scripts_indices = range(1, len(split_scripted_metric), 2)
+
+    scripts = [split_scripted_metric[i] for i in scripts_indices]
+    pos = 0
+    for i in scripts_indices:
+        split_scripted_metric[i] = '"place_holder_' + str(pos) + '"'
+        scripts[pos] = ' '.join(scripts[pos].split())
+        pos = pos + 1
+
+    scripted_metric = ''.join(split_scripted_metric)
+
+    scripted_metric_json = json.loads(scripted_metric)
+
+    for i in range(0, len(scripts)):
+        recursive_find_and_replace(scripted_metric_json, 'place_holder_' + str(i), scripts[i])
+
+    return scripted_metric_json
